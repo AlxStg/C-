0a48c849b57474833f5a490eb9a9ec2bea477a95
AlxStg <alex1985santiago@gmail.com>
2022-09-01 10:26:52 -0300

    Aula06 - Formatando a saída no console!

--- Aula05/Program.cs
+++ Aula05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Aula05 {
+    static void Main() {
+        int n1, n2, n3, n4, res;
+        n1 = 10;
+        n2 = 20;
+        n3 = 30;
+        n4 = 40;
+        res = n1 + n2 + n3 + n4;
+        Console.WriteLine("A soma de {0} + {1} + {2} + {3} = {4}", n1, n2, n3, n4, res);
+    }
+
+    
+}
+ 
---
+++ Aula06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Aula06 {
+    static void Main() {
+
+        double valorCompra=5.50, lucro=0.1, valorVenda=valorCompra+(valorCompra*lucro);
+        
+        string produto="Pastel";
+
+        Console.WriteLine("Produto.....: {0,10}", produto);
+        Console.WriteLine("Custo.......: {0,10:c}", valorCompra);
+        Console.WriteLine("Lucro % ....: {0,10:p}", lucro);
+        Console.WriteLine("Venda.......: {0,10:c}", valorVenda);
+        Console.WriteLine("Lucro $ ....: {0,10:c}", valorVenda-valorCompra);
+    }
+}
