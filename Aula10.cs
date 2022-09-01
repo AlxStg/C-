fc88ee0951488ceeff73b7322575a18649d00ebe
AlxStg <alex1985santiago@gmail.com>
2022-09-01 14:00:51 -0300

    10 - Enumeradores

---
+++ Aula10.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula10{
+
+    enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sabado};
+    static void Main(){
+
+      DiasSemana ds = (DiasSemana) 1;
+
+      int ds2 = ((int) DiasSemana.DiaSemana.ds)+1;
+
+      Console.WriteLine(ds + " é o " +ds2+ "º dia da semana");
+      
+
+    }
+}
