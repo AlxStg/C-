1d028be95132cc6a6b6f8d9a1739932232a14af6
AlxStg <alex1985santiago@gmail.com>
2022-09-01 10:51:31 -0300

    Recebendo dados do teclado

---
+++ Aula08/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Aula08 {
+    static void Main() {
+        int v1, v2, soma;
+
+        Console.WriteLine("Digite um numero: ");
+        v1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite outro numero: ");
+        v2 = int.Parse(Console.ReadLine());
+        soma = v1 + v2;
+        Console.WriteLine("A soma de {0} + {1} = {2}", v1, v2, soma);
+
+        
+    }
+}
