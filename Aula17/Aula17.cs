7b79d40b996823369831ffbc963d265653b82e84
AlxStg <alex1985santiago@gmail.com>
2022-09-07 23:45:36 -0300

    Array

---
+++ Aula17/Aula17.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula17 {
+    static void Main() {
+        
+        int[] n=new int[5];
+        n[0]=10;
+        n[1]=20;
+        n[2]=30;
+        n[3]=40;
+        n[4]=50;
+
+        Console.WriteLine(n[0]);
+    }
+}
