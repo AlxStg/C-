ec5a6539575606bda91321fb4b66a1d6d815965d
AlxStg <alex1985santiago@gmail.com>
2022-09-15 22:46:59 -0300

    While

---
+++ Aula20/Aula20.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula20{
+
+    static void Main() {
+        
+        int i = 10;
+        while(i>-1){
+            Console.WriteLine("Valor de i: {0}", i);
+            i--;
+        }
+        Console.WriteLine("FIM!!!");
+    }
+}
