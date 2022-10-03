b34c4d28e0c7fc7ae02ae31cb9e28d8db653b42e
AlxStg <alex1985santiago@gmail.com>
2022-10-03 16:18:02 -0300

    Cadeia de Herança e Construtor de classe base + operador ternário

--- Aula35/Aula35.cs
+++ Aula35/Aula35.cs
@@ -4,8 +4,11 @@ class Veiculo{ //Classe Base
 
     public int velMax;
     private bool ligado = false;
-    public int rodas;
+    private int rodas=4;
 
+    public Veiculo(int rodas){
+        this.rodas = rodas;
+    }
     public void ligar(){
         ligado = true;
     }
@@ -13,12 +16,12 @@ class Veiculo{ //Classe Base
         ligado = false;
     }
     public string getLigado(){
-        if(ligado){
-            return "sim";
-        }else{
-            return "não";
-        }
+        return (ligado?"sim":"não");
     }
+    public int getRodas(){
+        return rodas;
+    }
+
     
 
 }
@@ -26,14 +29,12 @@ class Veiculo{ //Classe Base
 class Carro:Veiculo{ // CLASSE DERIVADA
     public string nome;
     public string cor;
-    public string motorLigado;
-    public Carro(string nome, string cor){
+    public Carro(string nome, string cor):base(4){
         desligar();
-        rodas=4;
         velMax=120;
         this.nome = nome;
         this.cor = cor;
-        motorLigado = getLigado();
+        
          
         
     }
@@ -45,10 +46,10 @@ class Aula35{
 
     static void Main(){
         Carro c1=new Carro("Civic","Azul");
-        Console.WriteLine("\nModelo: {0},\nCor: {1},\nVelocidade Máxima: {2},\nRodas: {3}.\nLigado: {4}\n", c1.nome, c1.cor, c1.velMax, c1.rodas,c1.motorLigado);
+        Console.WriteLine("\nModelo: {0},\nCor: {1},\nVelocidade Máxima: {2},\nRodas: {3}.\nLigado: {4}\n", c1.nome, c1.cor, c1.velMax, c1.getRodas(),c1.getLigado());
 
         c1.ligar();
-        Console.WriteLine("\nModelo: {0},\nCor: {1},\nVelocidade Máxima: {2},\nRodas: {3}.\nLigado: {4}\n", c1.nome, c1.cor, c1.velMax, c1.rodas,c1.motorLigado);
+        Console.WriteLine("\nModelo: {0},\nCor: {1},\nVelocidade Máxima: {2},\nRodas: {3}.\nLigado: {4}\n", c1.nome, c1.cor, c1.velMax, c1.getRodas(),c1.getLigado());
 
     }
 }
