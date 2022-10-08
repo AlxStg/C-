19875eb5ed99c94c28711616a7eb4882036059b8
AlxStg <alex1985santiago@gmail.com>
2022-10-08 18:42:23 -0300

    Interface

--- Aula43/Aula43.cs
+++ Aula43/Aula43.cs
@@ -3,6 +3,7 @@ using System;
 class Aula43{
     static void Main(){
         Carro c1 = new Carro();
+        c1.info();
         c1.disparar();
 
     }
@@ -30,12 +31,19 @@ class Carro:Veiculo,Combate{
         this.ligado = false;
     }
     public void info(){
-
+    Console.WriteLine("O veiculo está ligado: {0}", motorLigado());
     }
     public void disparar(){
         Console.WriteLine("====");
         Console.WriteLine("BANG");
         Console.WriteLine("====");
+    }
+    public string motorLigado(){
+        if (ligado){
+            return  "Sim";
+        }else{
+            return "Não";
+        }
     }
 
 }
